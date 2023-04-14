5a31f9a25adad64ab65c025fe4304e4a531e0767
zyc233666 <1183413174@qq.com>
2023-04-14 21:21:07 +0800

    buid the 2d-analytical siginal in spatial domain

--- kk.cpp
+++ kk.cpp
@@ -12,15 +12,15 @@ void kramers_kronig(Mat& input_img, Mat& output_img)
     // 将输入图像转换为浮点型复数矩阵
     Mat complex_img;
     input_img.convertTo(complex_img, CV_32FC2);
-
+    input_img.convertTo(input_img, CV_32F);
     // 进行傅里叶变换，获取复数矩阵
     dft(complex_img, complex_img, DFT_COMPLEX_OUTPUT);
 
     // 分离实部和虚部
     Mat re, im;
     Mat plane[] = { re, im };
-    split(complex_img, plane);
-    fftshift(plane[0],plane[1]);
+    //split(complex_img, plane);
+    //fftshift(plane[0],plane[1]);
     //int cnt = 0;
     //Mat re_1d = Mat::zeros(1, plane[0].cols*plane[0].rows, CV_32F);
     //Mat im_1d = Mat::zeros(1, plane[0].cols * plane[0].rows, CV_32F);
@@ -56,7 +56,7 @@ void kramers_kronig(Mat& input_img, Mat& output_img)
     for (int i = 0; i < nr; i++) {
         if (i == nr/2)
         {
-            hilbert_kernel_r.at<float>(i, 0) = 1;
+            hilbert_kernel_r.at<float>(i, 0) = 0;
         }
         else
         {
@@ -65,20 +65,57 @@ void kramers_kronig(Mat& input_img, Mat& output_img)
         }
     }
 
-    for (int i = 0; i < nr; i++)
-    {
-        printf("%d: %f\n", i, hilbert_kernel_r.at<float>(i,0));
+    cv::Mat hilbert_kernel = Mat::zeros(input_img.size(), CV_32F);
+    int nx = hilbert_kernel.rows;
+    int ny = hilbert_kernel.cols;
+    double x, y;
+    for (int i = 0; i < nx; i++) {
+        for (int j = 0; j < ny; j++) {
+            if (i == nx / 2 && j == ny / 2) {
+                hilbert_kernel.at<float>(i, j) = 0;
+            }
+            else {
+                if (i == nx / 2) {
+                    x = 1;
+                }
+                else {
+                    x = i - nx / 2;
+                }
+                if (j == ny / 2) {
+                    y = 1;
+                }
+                else {
+                    y = j - ny / 2;
+                }
+                double distance = x * y;
+                hilbert_kernel.at<float>(i, j) = 1 / (CV_PI *CV_PI* distance);
+            }
+        }
     }
+
+    cv::Mat delta = Mat::zeros(input_img.rows, 1, CV_32F);
+    //hilbert_kernel_r.at<float>(0, 0) = 1;
+    int n = delta.rows;
+    for (int i = 0; i < nr; i++) {
+        if (i == n / 2) {
+            delta.at<float>(i, 0) = 1;
+        }
+    }
+
+    //for (int i = 0; i < nr; i++)
+    //{
+    //    printf("%d: %f\n", i, hilbert_kernel_r.at<float>(i,0));
+    //}
     // 对实部和虚部分别应用Hilbert核
-    filter2D(plane[0], hilbert_re, -1, hilbert_kernel_r);
-    filter2D(plane[1], hilbert_im, -1, hilbert_kernel_r);
-    fftshift(hilbert_re, hilbert_im);
+    filter2D(input_img, hilbert_re, -1, delta);
+    filter2D(input_img, hilbert_im, -1, hilbert_kernel_r);
+    //fftshift(hilbert_re, hilbert_im);
     Mat hilbert_v[] = { hilbert_re, hilbert_im };
     //hilbert_im = -hilbert_im;
     cv::Mat BLUR;
     cv::merge(hilbert_v, 2, BLUR);
-    cv::idft(BLUR, BLUR, DFT_COMPLEX_OUTPUT);
-    BLUR = BLUR / BLUR.rows / BLUR.cols;
+    //cv::idft(BLUR, BLUR, DFT_COMPLEX_OUTPUT);
+    //BLUR = BLUR / BLUR.rows / BLUR.cols;
     cv::split(BLUR, plane);
     imshow("0", plane[0] / 255);
     imshow("1", plane[1] / 255);
@@ -92,7 +129,7 @@ int main()
     //Mat output_img = Mat::zeros(input_img.size(), CV_32F);
     Mat output_img;
     kramers_kronig(input_img, output_img);
-    imshow("original", input_img);
+    imshow("original", input_img/255);
     //imshow("result", output_img);
     waitKey(0);
     system("pause");
